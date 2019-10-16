6d6114f1c06e668656abc4cbb63ca8d37c2040b9
wk-j <somnuk.wk@gmail.com>
2019-10-16 17:58:09 +0700

    Sensor

---
+++ src/Sonic/sketch_oct10a/sketch_oct10a.ino
@@ -0,0 +1,40 @@
+const int pingPin = 5;
+int inPin = 3;
+
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  long duration, cm;
+
+  pinMode(pingPin, OUTPUT);
+  pinMode(inPin, INPUT);
+
+
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pingPin, LOW);
+
+  duration = pulseIn(inPin, HIGH);
+
+  cm = microsecondsToCentimeters(duration);
+
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  delay(100);
+}
+
+long microsecondsToCentimeters(long microseconds)
+{
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the
+  // object we take half of the distance travelled.
+  return microseconds / 29 / 2;
+}
